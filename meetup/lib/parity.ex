0e20dba398d6ebbc5de81597c8263221ca99305b
Michael Hirsch <mdhirsch@gmail.com>
2017-07-05 21:44:09 -0600

    Added ability to time the parity computation

--- meetup/lib/parity.ex
+++ meetup/lib/parity.ex
@@ -25,4 +25,18 @@ defmodule Parity do
     odd_parity()
   end
   
+  def compute_parity(n, even_pid, odd_pid) do
+#  	even_pid = spawn(Parity, :even_parity, [])
+#  	odd_pid = spawn(Parity, :odd_parity, [])
+    send even_pid, {self(), even_pid, odd_pid, n}
+		receive do
+			{parity} -> 
+				"Result is #{inspect(parity)}"
+		end
+  end
+  
+  def compute(even_pid, odd_pid, n) do
+		IO.puts inspect :timer.tc(Parity, :compute_parity, [n, even_pid, odd_pid])
+  end
+  
 end
